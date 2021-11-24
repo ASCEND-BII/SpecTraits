5c19a8e0280b497199cfefed498399df4220f52e
J. Antonio Guzmán Q <antguz06@gmail.com>
2021-11-24 17:17:09 -0600

    Plot spectra helper function

--- R/spectra_plot.R
+++ R/spectra_plot.R
@@ -2,26 +2,33 @@
 ##### Spectra plot function
 ################################################################################
 
-spectra_plot <- function(load_spectra) {
+#Spectra plot
+spectra_figure <- function(input) {
   
-  frame <- load_spectra
+  #Load
+  spectra_frame <- spectra_load(input)
   
   #Melt to plot each spectrum
-  frame_melt <- melt(frame, 
-                     id.vars = c("ID"),
-                     measure.vars = c(.SD),
-                     variable.name = "Wavelength",
-                     value.name = "Reflectance")
+  frame_melt <- spectra_frame %>% reshape2::melt(id.vars = "ID", 
+                                                 variable = "Wavelength",
+                                                 value.name = "Reflectance")  
   
   #Transform to number
   frame_melt$Wavelength <- as.numeric(as.character(frame_melt$Wavelength))
   
   #Get spectra summary
-  frame_summary <- frame_melt[, list(mean = mean(Reflectance),
-                                     sd = sd(Reflectance),
-                                     min = min(Reflectance),
-                                     max = max(Reflectance)), 
-                                     by = .(Wavelength)]
+  frame_summary <- frame_melt %>% 
+    group_by(Wavelength) %>% 
+    summarize(mean = mean(Reflectance),
+              sd = sd(Reflectance),
+              min = min(Reflectance),
+              max = max(Reflectance))
+  
+  #Transform to number
+  frame_summary$Wavelength <- as.numeric(as.character(frame_summary$Wavelength))
+  
+  #X limits
+  x_limits <- range(frame_summary$Wavelength)
   
   #Plotting element
   plot <- ggplot() +
@@ -37,11 +44,13 @@ spectra_plot <- function(load_spectra) {
     geom_line(data  = frame_summary, 
               aes(x = Wavelength, y = mean),
               colour = "#0097a7ff") +
+    xlab("Reflectance") + ylab("Wavelength (nm)") +
     scale_x_continuous(limits = x_limits, expand = c(0, 0)) +
     scale_y_continuous(limits = c(0, 1), expand = c(0, 0)) +
-    theme_classic()
+    theme_bw(base_size = 14) +
+    theme(plot.margin = margin(t = 20, r = 20, b = 0, l = 0, unit = "pt"))
   
-  export(plot)
+  return(plot)
   
 }
 
