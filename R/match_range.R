c31a67e48724eb701a3036c73cbfb32ee2759240
J. Antonio Guzmán Q <antguz06@gmail.com>
2021-12-08 14:55:24 -0600

    match_range modification

--- R/match_range.R
+++ R/match_range.R
@@ -2,21 +2,21 @@
 ##### Match spectral range
 ################################################################################
 
-match_range <- function(spectra_frame, range_spectra) {
+match_range <- function(spectra_frame, coeff_range) {
 
   #Get range
   wv <- as.numeric(colnames(spectra_frame)[-1])
 
   #Error if bands are not available
-  if(min(wv) >= range_spectra[1] | max(wv) <= range_spectra[2]) {
+  if(min(wv) >= coeff_range[1] | max(wv) <= coeff_range[2]) {
 
     stop("The uploaded spectra does not match the spectral
          range required to predict leaf tratis")
   }
 
   #Current ranges
-  lower_range <- wv >= range_spectra[1]
-  upper_range <- wv <= range_spectra[2]
+  lower_range <- wv >= coeff_range[1]
+  upper_range <- wv <= coeff_range[2]
 
   to_subset <- lower_range & upper_range
   to_subset <- c(FALSE, to_subset) #add ID as FALSE
