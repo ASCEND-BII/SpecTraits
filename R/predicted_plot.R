516c429344607108c99096ca4176d37aa9c81a9a
J. Antonio Guzmán Q <antguz06@gmail.com>
2021-12-08 14:56:09 -0600

    predicted plot histogram inseat of density plot

--- R/predicted_plot.R
+++ R/predicted_plot.R
@@ -31,10 +31,10 @@ predicted_plot <- function(frame) {
 
   #Plotting element
   plot <- ggplot(predicted_frame, aes(x= predicted)) +
-    geom_histogram(aes(y= ..density..), fill="#0097a7ff",
+    geom_histogram(fill="#0097a7ff",
                    color = "grey95", position="identity") +
-    geom_density(alpha=.2, fill="grey80") +
-    ylab("Density") + xlab("Predicted") +
+    ylab("Frequency") + xlab("Predicted") +
+    scale_y_continuous(expand = c(0,0))+
     theme_bw(base_size = 14) +
     theme(plot.margin = margin(t = 20, r = 20, b = 0, l = 0, unit = "pt"))
 
