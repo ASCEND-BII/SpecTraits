0f41e1f18a115d594fd0b9dc4a967654b3f8291b
J. Antonio Guzmán Q <antguz06@gmail.com>
2022-01-11 12:30:45 -0600

    Implementing traits_export

---
+++ R/traits_export.R
@@ -0,0 +1,31 @@
+################################################################################
+##### Donwload predicted traits
+################################################################################
+
+################################################################################
+#UI
+traits_export_ui <- function(frame, label = "Download predicted traits") {
+  # `NS(frame)` returns a namespace function, which was save as `ns` and will
+  # invoke later.
+  ns <- NS(frame)
+
+  tagList(
+    downloadButton(ns("downloadData"), "Download")
+  )
+}
+
+################################################################################
+#Server
+traits_export_server <- function(input, output, session, data) {
+
+  output$downloadData<- downloadHandler(
+    filename = function() {
+      paste0("predicted-traits", ".csv")
+    },
+    content = function(file) {
+      write.csv(data(), file, row.names = FALSE)
+    }
+  )
+}
+
+
