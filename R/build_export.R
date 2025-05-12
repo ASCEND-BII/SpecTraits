c817820cfa46ce1cd673dac9cc19fa823a037bb3
Guzmán Q. <aguzman@fas.harvard.edu>
2025-05-12 16:33:38 -0400

    trying to fix the export

--- R/build_export.R
+++ R/build_export.R
@@ -41,38 +41,54 @@ build_export_server <- function(export,
 
         # Temporary files for CSVs
         tmpdir <- tempdir()
-        # trait_selector_file <- file.path(tmpdir, paste0("RMSEP_", trait_selector(), ".csv"))
 
         # Build import figure
         build_import_figure_file1 <- file.path(tmpdir, paste0("spectra_", trait_selector(), ".jpeg"))
+        jpeg(build_import_figure_file1, width = 90, height = 60, units = "mm", res = 300)
+        build_import_figure()$spectra_figure
+        dev.off()
+
         build_import_figure_file2 <- file.path(tmpdir, paste0("histogram_", trait_selector(), ".jpeg"))
-        # ggsave(build_import_figure_file1, plot = build_import_figure()$spectra_figure, width = 90, height = 60, units = "mm", dpi = 300)
-        # ggsave(build_import_figure_file2, plot = build_import_figure()$trait_figure, width = 90, height = 60, units = "mm", dpi = 300)
+        jpeg(build_import_figure_file2, width = 90, height = 60, units = "mm", res = 300)
+        build_import_figure()$trait_figure
+        dev.off()
 
         # Split
-        # split_method_file <- file.path(tmpdir, paste0("RMSEP_", trait_selector(), ".csv"))
         split_action_figure_file1 <- file.path(tmpdir, paste0("spectra-split_", trait_selector(), ".jpeg"))
+        jpeg(split_action_figure_file1, width = 90, height = 60, units = "mm", res = 300)
+        split_action_figure()$spectra_split_figure
+        dev.off()
+
         split_action_figure_file2 <- file.path(tmpdir, paste0("trait-split_", trait_selector(), ".jpeg"))
-        ggsave(split_action_figure_file1, plot = split_action_figure()$spectra_split_figure, width = 90, height = 60, units = "mm", dpi = 300)
-        ggsave(split_action_figure_file2, plot = split_action_figure()$trait_split_figure, width = 90, height = 60, units = "mm", dpi = 300)
+        jpeg(split_action_figure_file2, width = 90, height = 60, units = "mm", res = 300)
+        split_action_figure()$trait_split_figure
+        dev.off()
 
         # Press
-        # press_method_file <- file.path(tmpdir, paste0("RMSEP_", trait_selector(), ".csv"))
         press_frame_file <- file.path(tmpdir, paste0("RMSEP_", trait_selector(), ".csv"))
-        press_action_figure_file <- file.path(tmpdir, paste0("RMSEP-components_", trait_selector(), ".jpeg"))
         fwrite(press_frame()$rmsep, press_frame_file)
-        ggsave(press_action_figure_file, plot = press_action_figure, width = 90, height = 60, units = "mm", dpi = 300)
+
+        press_action_figure_file <- file.path(tmpdir, paste0("RMSEP-components_", trait_selector(), ".jpeg"))
+        jpeg(press_action_figure_file, width = 90, height = 60, units = "mm", res = 300)
+        press_action_figure()
+        dev.off()
 
         # Final model
-        # final_method_file <- file.path(tmpdir, paste0("RMSEP_", trait_selector(), ".csv"))
         final_PLSR_file1 <- file.path(tmpdir, paste0("coefficients_", trait_selector(), ".csv"))
-        final_PLSR_file2 <- file.path(tmpdir, paste0("vip_", trait_selector(), ".csv"))
-        coefficients_figure_file1 <- file.path(tmpdir, paste0("coefficients_", trait_selector(), ".jpeg"))
-        vip_figure_file2 <- file.path(tmpdir, paste0("vip_", trait_selector(), ".jpeg"))
         fwrite(final_PLSR()$coefficients, final_PLSR_file1)
+
+        final_PLSR_file2 <- file.path(tmpdir, paste0("vip_", trait_selector(), ".csv"))
         fwrite(final_PLSR()$vip, final_PLSR_file2)
-        ggsave(coefficients_figure_file1, plot = coefficients_figure()$coefficients, width = 90, height = 60, units = "mm", dpi = 300)
-        ggsave(vip_figure_file2, plot = coefficients_figure()$vip, width = 90, height = 60, units = "mm", dpi = 300)
+
+        coefficients_figure_file1 <- file.path(tmpdir, paste0("coefficients_", trait_selector(), ".jpeg"))
+        jpeg(coefficients_figure_file1, width = 90, height = 60, units = "mm", res = 300)
+        coefficients_figure()$coefficients
+        dev.off()
+
+        vip_figure_file2 <- file.path(tmpdir, paste0("vip_", trait_selector(), ".jpeg"))
+        jpeg(vip_figure_file2, width = 90, height = 60, units = "mm", res = 300)
+        coefficients_figure()$vip
+        dev.off()
 
         # Observed predicted frame
         results_predict_file <- file.path(tmpdir, paste0("observed-predicted_", trait_selector(), ".csv"))
@@ -80,22 +96,40 @@ build_export_server <- function(export,
 
         # Performance train
         perf_train_figure_file1 <- file.path(tmpdir, paste0("training_observed-predicted_", trait_selector(), ".jpeg"))
+        jpeg(perf_train_figure_file1, width = 90, height = 60, units = "mm", res = 300)
+        perf_train_figure()$obser_pred
+        dev.off()
+
         perf_train_figure_file2 <- file.path(tmpdir, paste0("training_histogram_", trait_selector(), ".jpeg"))
+        jpeg(perf_train_figure_file2, width = 90, height = 60, units = "mm", res = 300)
+        perf_train_figure()$histogram
+        dev.off()
+
         perf_train_figure_file3 <- file.path(tmpdir, paste0("training_residuals_", trait_selector(), ".jpeg"))
+        jpeg(perf_train_figure_file3, width = 90, height = 60, units = "mm", res = 300)
+        perf_train_figure()$residuals
+        dev.off()
+
         perf_train_figure_file4 <- file.path(tmpdir, paste0("training_performance_", trait_selector(), ".csv"))
-        ggsave(perf_train_figure_file1, plot = perf_train_figure()$obser_pred, width = 90, height = 60, units = "mm", dpi = 300)
-        ggsave(perf_train_figure_file2, plot = perf_train_figure()$histogram, width = 90, height = 60, units = "mm", dpi = 300)
-        ggsave(perf_train_figure_file3, plot = perf_train_figure()$residuals, width = 90, height = 60, units = "mm", dpi = 300)
         fwrite(perf_train_figure()$performance, perf_train_figure_file4)
 
         # Performance testing
         perf_test_figure_file1 <- file.path(tmpdir, paste0("testing_observed-predicted_", trait_selector(), ".jpeg"))
+        jpeg(perf_test_figure_file1, width = 90, height = 60, units = "mm", res = 300)
+        perf_test_figure()$obser_pred
+        dev.off()
+
         perf_test_figure_file2 <- file.path(tmpdir, paste0("testing_histogram_", trait_selector(), ".jpeg"))
+        jpeg(perf_test_figure_file2, width = 90, height = 60, units = "mm", res = 300)
+        perf_test_figure()$histogram
+        dev.off()
+
         perf_test_figure_file3 <- file.path(tmpdir, paste0("testing_residuals_", trait_selector(), ".jpeg"))
+        jpeg(perf_test_figure_file3, width = 90, height = 60, units = "mm", res = 300)
+        perf_test_figure()$residuals
+        dev.off()
+
         perf_test_figure_file4 <- file.path(tmpdir, paste0("testing_performance_", trait_selector(), ".csv"))
-        ggsave(perf_test_figure_file1, plot = perf_test_figure()$obser_pred, width = 90, height = 60, units = "mm", dpi = 300)
-        ggsave(perf_test_figure_file2, plot = perf_test_figure()$histogram, width = 90, height = 60, units = "mm", dpi = 300)
-        ggsave(perf_test_figure_file3, plot = perf_test_figure()$residuals, width = 90, height = 60, units = "mm", dpi = 300)
         fwrite(perf_test_figure()$performance, perf_test_figure_file4)
 
         # Create ZIP archive
